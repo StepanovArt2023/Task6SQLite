34878a53dc9b8b0291d011a83fa4547521063cc8
artem <artem@DESKTOP-7S1DBR4>
2023-04-26 23:22:18 +0300

    Добавьте файлы проекта.

---
+++ Task6SQLite/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6SQLite.Model
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int RoleID { get; set; }
+        public virtual Role Role { get; set; }
+    }
+}
